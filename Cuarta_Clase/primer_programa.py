05debfd0c30618187c514229d4a41ef3af37f627
luisclaret <luisclaretafanador@gmail.com>
2025-12-27 22:03:45 -0500

    quinta clase

---
+++ Cuarta_Clase/primer_programa.py
@@ -0,0 +1,12 @@
+# Calculador
+
+# suma, resta, multiplicacion, division, exponencia y modulo.
+# Solo operaciones entre dos numeros
+
+
+def introducir_numero():
+    x = input("Introduzca numero: ")
+    return x
+
+
+x_1 = introducir_numero()
